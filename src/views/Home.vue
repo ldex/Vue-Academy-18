577b39206d231533fa87f04c4fb1565287ba97ea
ldex <laurent.duveau+git@gmail.com>
2021-10-19 15:39:50 -0400

    Teleport

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="home">
+    <modal-button button-text="Open Modal!"></modal-button>
     <img alt="Vue logo" src="../assets/logo.png">
   </div>
 </template>
 
 <script>
+import ModalButton from '../components/ModalButton.vue'
 
 export default {
   name: 'Home',
   components: {
+    ModalButton
   }
 }
 </script>
+
+    ModalButton
